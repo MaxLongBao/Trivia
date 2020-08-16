a9f1634099ef0d174bd559f01466628a2a8d4c04
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2020-08-16 14:59:57 +0200

    Updates

---
+++ precache-manifest.a75217f27db7667bad50d70ac09a6519.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "face2e8d60ace7bececde3d839c9c253",
+    "url": "/Trivia/index.html"
+  },
+  {
+    "revision": "a7a20f3571c9ef2cf714",
+    "url": "/Trivia/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "10f5ca9c671c71b94071",
+    "url": "/Trivia/static/js/2.96a1e1c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Trivia/static/js/2.96a1e1c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7a20f3571c9ef2cf714",
+    "url": "/Trivia/static/js/main.2cf7082e.chunk.js"
+  },
+  {
+    "revision": "813061b9eea68719fc0c",
+    "url": "/Trivia/static/js/runtime-main.d1b2b455.js"
+  }
+]);
