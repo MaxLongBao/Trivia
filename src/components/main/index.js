80393ff0a52bd6e6152ec86f61855eb836c46b46
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2020-11-24 22:14:16 +0000

    api request in Main

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Topics from '../topics';
 import Level from '../level';
 import Question from '../question';
 
 const Main = () => {
+
+  const [question, setQuestion] = useState();
+
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=1&category=22&difficulty=medium&type=multiple')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result.results[0]);
+          setQuestion(result.results[0].question);
+        }
+      )
+  }, []);
+
   return (
     <div>
       Trivia App
       <Topics />
       <Level />
-      <Question />
+      <Question question={question} />
     </div>
   )
 };
--- src/components/question/index.js
+++ src/components/question/index.js
@@ -1,19 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const Question = (props) => {
   
-  const [question, setQuestion] = useState();
+  const {question} = props;
 
-  useEffect(() => {
-    fetch('https://opentdb.com/api.php?amount=1&category=22&difficulty=medium&type=multiple')
-      .then(res => res.json())
-      .then(
-        (result) => {
-          console.log(result.results[0]);
-          setQuestion(result.results[0].question);
-        }
-      )
-  }, []);
   return(
     <div>
       {question}
